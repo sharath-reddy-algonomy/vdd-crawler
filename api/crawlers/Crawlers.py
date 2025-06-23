e84d51df671ae142d3b5b6597fbb81a9e9d4aa22
sharath-reddy-algonomy <sharath.reddy@algonomy.com>
2025-06-23 16:30:03 +0530

    Modified the search term to check if the results are closer to that of manual searches.

--- api/crawlers/Crawlers.py
+++ api/crawlers/Crawlers.py
@@ -10,16 +10,16 @@ logger = logging.getLogger("crawler")
 
 def get_search_term(actor_name) -> str:
     return (
-        f'"{actor_name}" & AND ("facilitation payment" | litigation | judicial | fine | launder | OFAC | '
+        f'"{actor_name}" "facilitation payment" | litigation | judicial | fine | launder | OFAC | '
         f'terror | manipulate | counterfeit | traffic | court | appeal | investigate | guilty | illegal | '
         f'arrest | evasion | sentence | kickback | prison | jail | corruption | corrupt | "grease payment" '
-        f'| crime | bribe | fraud | condemn | accuse | implicate)')
+        f'| crime | bribe | fraud | condemn | accuse | implicate')
 
 
 def get_hindi_search_term(actor_name):
-    return (f'"{actor_name}" AND (अपराध | रिश्वत | धोखाधड़ी | निंदा | आरोप | शामिल | ग्रेस भुगतान | '
+    return (f'"{actor_name}" अपराध | रिश्वत | धोखाधड़ी | निंदा | आरोप | शामिल | ग्रेस भुगतान | '
             f'मुकदमा | न्यायिक | जुर्माना | मनी लॉन्ड्रिंग | आतंकवाद | नकली | तस्करी | कोर्ट | '
-            f'अपील | जांच | दोषी | अवैध | गिरफ्तारी | चोरी | सजा | घूस | जेल | भ्रष्टाचार)')
+            f'अपील | जांच | दोषी | अवैध | गिरफ्तारी | चोरी | सजा | घूस | जेल | भ्रष्टाचार')
 
 
 class BaseCrawler(ABC):
@@ -102,10 +102,10 @@ class OfficialWebsiteCrawler(BaseCrawler):
     async def crawl(self, actor_name, directors, schedule_id, pages, site_url: None):
         google_page = CrawlerPage()
         logger.info(f"Performing official website search for {actor_name} on site {site_url}...")
-        search_term = f'site:{site_url} "{actor_name}" & AND ("facilitation payment" | litigation | judicial | fine | launder | OFAC | '
+        search_term = f'site:{site_url} "{actor_name}" "facilitation payment" | litigation | judicial | fine | launder | OFAC | '
         f'terror | manipulate | counterfeit | traffic | court | appeal | investigate | guilty | illegal | '
         f'arrest | evasion | sentence | kickback | prison | jail | corruption | corrupt | "grease payment" '
-        f'| crime | bribe | fraud | condemn | accuse | implicate)'
+        f'| crime | bribe | fraud | condemn | accuse | implicate'
         await google_page.search_and_download(search_term, pages, f"{schedule_id}/{self.get_category()}",
                                               search_url=self.get_search_engine_url())
         logger.info("Completed official website search")
